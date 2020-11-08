1b7a1cf51ce8b18aeb19a7f46ac6362b66126585
Markus Moenig <markus@moenig.io>
2020-11-08 15:14:55 +0700

    Initial Commit

---
+++ DenrimPlayer macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  DenrimPlayer macOS
+//
+//  Created by Markus Moenig on 8/11/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
